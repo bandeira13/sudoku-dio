3733b01c641a66029ff03a94f86418c4ed000bbc
bandeira13 <malubsouza@yahoo.com.br>
2025-08-13 15:59:07 -0300

    Encapsulamento das informações necessárias para uma única célula ( um número fixo não pode ser alterado).

--- src/br/com/sudoku/model/Board.java
+++ src/br/com/sudoku/model/Board.java
@@ -1,4 +1,78 @@
 package br.com.sudoku.model;
 
+import br.com.sudoku.util.BoardTemplate;
+
+import java.util.stream.IntStream;
+
 public class Board {
+
+    private Space[][] spaces;
+
+    public Board() {
+        this.spaces = new Space[9][9];
+        // Adicione aqui a lógica para carregar um tabuleiro inicial, por exemplo:
+        // int[][] initialBoard = { ... };
+        // for (int r = 0; r < 9; r++) {
+        //     for (int c = 0; c < 9; c++) {
+        //         spaces[r][c] = new Space(initialBoard[r][c]);
+        //     }
+        // }
+    }
+
+    public Space getSpace(int row, int col) {
+        return spaces[row][col];
+    }
+
+    public boolean isValid(int row, int col, int value) {
+        return isRowValid(row, value) && isColValid(col, value) && isBoxValid(row, col, value);
+    }
+
+    private boolean isRowValid(int row, int value) {
+        for (int col = 0; col < 9; col++) {
+            if (spaces[row][col].getValue() == value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isColValid(int col, int value) {
+        for (int row = 0; row < 9; row++) {
+            if (spaces[row][col].getValue() == value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isBoxValid(int row, int col, int value) {
+        int boxRowStart = row - row % 3;
+        int boxColStart = col - col % 3;
+        for (int r = boxRowStart; r < boxRowStart + 3; r++) {
+            for (int c = boxColStart; c < boxColStart + 3; c++) {
+                if (spaces[r][c].getValue() == value) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isComplete() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (spaces[row][col].isEmpty()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void printBoard() {
+        Object[] values = IntStream.range(0, 81)
+                .mapToObj(i -> spaces[i / 9][i % 9].toString())
+                .toArray();
+        System.out.println(String.format(BoardTemplate.BOARD_TEMPLATE, values));
+    }
 }
--- src/br/com/sudoku/model/Space.java
+++ src/br/com/sudoku/model/Space.java
@@ -1,4 +1,35 @@
 package br.com.sudoku.model;
 
 public class Space {
+
+    private int value;
+    private boolean isFixed;
+
+    public Space(int value) {
+        this.value = value;
+        this.isFixed = (value != 0); // 0 indica um espaço vazio
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if (!isFixed) {
+            this.value = value;
+        }
+    }
+
+    public boolean isFixed() {
+        return isFixed;
+    }
+
+    public boolean isEmpty() {
+        return value == 0;
+    }
+
+    @Override
+    public String toString() {
+        return isEmpty() ? " " : String.valueOf(value);
+    }
 }
