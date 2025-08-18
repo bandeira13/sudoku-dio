2857429fbb3f41aeff0861be296754b78acc2ff4
bandeira13 <malubsouza@yahoo.com.br>
2025-08-18 13:45:27 -0300

    Implementação da limitação de números

--- src/br/com/sudoku/ui/NumberFilter.java
+++ src/br/com/sudoku/ui/NumberFilter.java
@@ -1,4 +1,26 @@
 package br.com.sudoku.ui;
+import javax.swing.text.*;
 
-public class NumberFilter {
+public class NumberFilter extends DocumentFilter {
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+        if (isValid(string, fb.getDocument().getLength())) {
+            super.insertString(fb, offset, string, attr);
+        }
+    }
+    @Override
+    public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs)
+            throws BadLocationException {
+        if (isValid(text, fb.getDocument().getLength() - length)) {
+            super.replace(fb, offset, length, text, attrs);
+        }
+    }
+
+    private boolean isValid(String text, int currentLength) {
+        // só aceita um único número de 1 a 9
+        return text.matches("[1-9]") && currentLength == 0;
+    }
+
+
+
 }
--- src/br/com/sudoku/ui/SudokuUi.java
+++ src/br/com/sudoku/ui/SudokuUi.java
@@ -1,4 +1,52 @@
 package br.com.sudoku.ui;
 
-public class SudokuUi {
+import br.com.sudoku.model.Board;
+
+
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.text.AbstractDocument;
+
+public class SudokuUi extends JFrame {
+    private Board board;
+    private JTextField[][] cells = new JTextField[9][9];
+
+    public SudokuUi(Board board) {
+        this.board = board;
+        setTitle("Sudoku");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLayout(new GridLayout(9, 9));
+
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                cells[row][col] = new JTextField();
+                cells[row][col].setHorizontalAlignment(JTextField.CENTER);
+
+                int value = board.getSpace(row, col).getValue();
+                if (value != 0) {
+                    cells[row][col].setText(String.valueOf(value));
+                    cells[row][col].setEditable(false);
+                    cells[row][col].setBackground(Color.LIGHT_GRAY);
+                } else {
+                    ((AbstractDocument)cells [row][col].getDocument()).setDocumentFilter(new NumberFilter());
+                    if (board.getSpace(row, col).isWrong()) {
+                        cells[row][col].setBackground(Color.RED);
+                    } else {
+                        cells[row][col].setBackground(Color.WHITE);
+                    }
+
+                }
+
+                add(cells[row][col]);
+            }
+        }
+
+        setVisible(true);
+
+
+    }
+
 }
